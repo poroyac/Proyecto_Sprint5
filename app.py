df748f5d70fb5883ec007d820732f163b566835a
Pamela Oroya <pame.oroya@gmail.com>
2024-05-29 17:30:25 -0500

    Desarrollo de cuadros de mandos

---
+++ app.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+car_data = read_csv('vehicles.csv') #leer los datos
+
+st.header('Dashboard de Vehiculos')
+
+hist_button = st.button('Construir un histograma') #crear un botón
+
+if hist_button: #al hacer clic en el botón
+    #escribir mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches.')
+    
+    #crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+
+    #mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+disp_button = st.button('Construir gráfico de dispersión')
+
+if disp_button: #al hacer clic en el botón
+    #escribir mensaje
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches.')
+    
+    #crear gráfico de dispersión
+    fig = px.scatter(car_data, x="odometer", y="price")
+    
+    #mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
