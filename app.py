a0666ff176f5acc37090eab6e300fbdaeb663fff
Pamela Oroya <pame.oroya@gmail.com>
2024-06-10 20:12:55 -0500

    programa python

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ hist_button = st.button('Construir un histograma') #crear un botón
 
 if hist_button: #al hacer clic en el botón
     #escribir mensaje
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches.')
+    st.write('Histograma de Vehiculos de anuncios de venta de coches.')
     
     #crear un histograma
     fig = px.histogram(car_data, x="odometer")
@@ -22,7 +22,7 @@ disp_button = st.button('Construir gráfico de dispersión')
 
 if disp_button: #al hacer clic en el botón
     #escribir mensaje
-    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches.')
+    st.write('Gráfico de dispersión de anuncios de venta de coches.')
     
     #crear gráfico de dispersión
     fig = px.scatter(car_data, x="odometer", y="price")
